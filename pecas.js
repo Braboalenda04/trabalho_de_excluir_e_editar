39860844a05aa988fc11d362c586fcf4f2546c24
Gabriel R <r.santos52@alunoescolassociais.org.br>
2023-08-08 09:59:56 -0300

    Cria uma pagina de Lista de peças

--- pecas.js
+++ pecas.js
@@ -1,40 +1,71 @@
-function Validar(){
-    let email = document.getElementById('loginEmail').value;
-    let senha = document.getElementById('loginSenha').value;
-    if(!email || !senha){
-        alert("Favor preencha os campos para acessar o site");
-    }
-    else{
-        window.location.href = "index.html";
-    }
-}
-// cria lista de usuario dentro de um array
+//CRIA LISTA DE PEÇAS DENTRO DE UM ARRAY
 var dadosLista = [];
-function salvarUser(){
+function salvarUser() {
     let nomeUser = document.getElementById("nomeUser").value;
-    if(nomeUser){
-        dadosLista.push(nomeUser);
+    let precoUser = parseFloat(document.getElementById("precoInput").value);
+    if (nomeUser && !isNaN(precoUser)) {
+        dadosLista.push({ nome: nomeUser, preco: precoUser });
         criaLista();
         document.getElementById("nomeUser").value = '';
-    }
-    else{
-        alert("Usuário, por favor preencher o campo Nome");
-    }
-}
-// funcão para criação de lista na tabela
-function criaLista(){
-    let tabela = document.getElementById('tabela').innerHTML = "<tr><th>Nome Usuário</th><th>Ações</th></tr>";
-    // laço de repetição para colocar nomes na tabela
-    for(let i=0;i <= (dadosLista.length-1);i++){
-        tabela += "<tr><td>" + dadosLista[i] + "</td><td><button class='btn btn-success' onclick='editar(this.parentNode.parentNode.rowIndex)'>Editar</button><button class='btn btn-danger' onclick='excluir(this.parentNode.parentNode.rowIndex)'>Excluir</button></td></tr>";
-        document.getElementById("tabela").innerHTML = tabela;
+        document.getElementById("precoInput").value = '';
+    } else {
+        alert("Preencha o nome e/ou preço corretamente");
     }
 }
-function editar(i){
-    document.getElementById("nomeUser").value = dadosLista[(i - 1)];
-    dadosLista.splice(dadosLista[(i - 1)], 1);
+// FUNÇÃO PARA A CRIAÇÃO DE LISTA NA TABELA
+function criaLista() {
+    let tabela = document.getElementById("tabela");
+    tabela.innerHTML = `
+        <tr>
+            <th>Nome Usuário</th>
+            <th>Preço</th>
+            <th>Ações</th>
+        </tr>`;
+    for (let i = 0; i < dadosLista.length; i++) {
+
+        tabela.innerHTML += `
+
+            <tr>
+
+                <td>${dadosLista[i].nome}</td>
+
+                <td>R$ ${dadosLista[i].preco.toFixed(2)}</td>
+
+                <td>
+
+                    <button class='btn btn-success' onclick='editar(${i})'>Editar</button>
+
+                    <button class='btn btn-danger' onclick='excluir(${i})'>Excluir</button>
+
+                </td>
+            </tr>`;
+    }
 }
-function excluir(i){
-    dadosLista.splice((i - i), 1);
-    document.getElementById('tabela').deleteRow(i);
+// FUNÇÃO PARA EDITAR PEÇA
+
+function editar(i) {
+    document.getElementById("nomeUser").value = dadosLista[i].nome;
+    document.getElementById("precoInput").value = dadosLista[i].preco;
+    dadosLista.splice(i, 1);
+    criaLista();
+}
+// FUNÇÃO PARA EXCLUIR PEÇA
+function excluir(i) {
+    dadosLista.splice(i, 1);
+    criaLista();
+}
+// FUNÇÃO PARA CALCULAR TOTAL
+
+function calcularTotal() {
+
+    let total = 0;
+
+    for (let i = 0; i < dadosLista.length; i++) {
+
+        total += dadosLista[i].preco;
+
+    }
+
+    document.getElementById("total").textContent = total.toFixed(2);
+
 }
